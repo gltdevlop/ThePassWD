7f54887ac1098fa3fd7b1561647ce05b46834482
gltdevlop <99366409+gltdevlop@users.noreply.github.com>
2024-09-28 19:44:21 +0200

    ui updated

--- passwd-gui.py
+++ passwd-gui.py
@@ -1,3 +1,4 @@
+import os
 import tkinter
 import random
 import string
@@ -6,6 +7,10 @@ import ctypes
 
 def gen_passwd():
     word_in_password = word.get()
+
+    if len(word_in_password) < 7:
+        ctypes.windll.user32.MessageBoxW(0, "7 Chars Needed, please reboot the software", "Error", 0x10)
+        quit()
 
     char_list = list(word_in_password)
     char_list[2], char_list[6], char_list[4] = char_list[6], char_list[4], char_list[2]
@@ -25,19 +30,19 @@ root = tkinter.Tk()
 root.title("ThePassWD")
 root.geometry('450x600')
 
-word_text = tkinter.Label(root, text="Enter a 7 letters min word")
-word_text.place(x=160,y=250)
+word_text = tkinter.Label(root, text="Enter a 7 letters min word", font=("Helvetica",15))
+word_text.place(x=110,y=250)
 
-word = tkinter.Entry(root)
-word.place(x=170, y=300)
+word = tkinter.Entry(root, width=40)
+word.place(x=105, y=300)
 
 generate_button = tkinter.Button(root, text="Generate", command=gen_passwd)
-generate_button.place(x=170, y=350)
+generate_button.place(x=200, y=330)
 
 passwd = tkinter.StringVar(root)
 
 passwd2 = tkinter.Entry(root, textvariable=passwd, width=40)
-passwd2.place(x=130, y=400)
+passwd2.place(x=105, y=400)
 
 root.mainloop()
 
